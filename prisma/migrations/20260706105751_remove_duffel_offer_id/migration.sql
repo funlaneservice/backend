7962475367933745e93e187f3cc1a9a837f3add1
DevEmmy <eolaosebikan60@gmail.com>
2026-07-09 14:14:35 +0100

    feat: remove duffelOfferId column from quote_options table

---
+++ prisma/migrations/20260706105751_remove_duffel_offer_id/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `duffelOfferId` on the `quote_options` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "quote_options" DROP COLUMN "duffelOfferId";
